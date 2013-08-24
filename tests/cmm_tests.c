834b00a93425e4e5a070039ca4a2e379334aa71c
Oleg Ivanov <iosmsft@outlook.com>
2013-08-24 19:18:21 +0300

    added first test

---
+++ tests/cmm_tests.c
@@ -0,0 +1,17 @@
+#include "minunit.h"
+#include <cmm/cmm.h>
+
+char *test()
+{
+	return NULL;
+}
+
+char *all_tests() {
+	mu_suite_start();
+
+	mu_run_test(test);
+
+	return NULL;
+}
+
+RUN_TESTS(all_tests);
