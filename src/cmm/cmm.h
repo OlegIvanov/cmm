28176f5679387217cd9cc324f43adf949bebe9fa
Oleg Ivanov <iosmsft@outlook.com>
2013-08-24 23:24:21 +0300

    reference counting

--- src/cmm/cmm.h
+++ src/cmm/cmm.h
@@ -8,32 +8,19 @@ typedef struct Header {
 	uint32_t ref_count;	
 } Header;
 
-typedef struct ObjectWrapper {
-	Header *header;
-	void *object;
-} ObjectWrapper;
-
 #define NEW(Type) Object_new(sizeof(Type))
 
 static inline void *Object_new(size_t object_size)
 {
-	ObjectWrapper *wrapper = calloc(1, sizeof(ObjectWrapper));
-	check_mem(wrapper);
-	
-	wrapper->header = calloc(1, sizeof(Header));
-	check_mem(wrapper->header);
+	Header *header = calloc(1, sizeof(Header) + object_size);
+	check_mem(header);
 
-	wrapper->object = calloc(1, sizeof(object_size));
-	check_mem(wrapper->object);
+	header->ref_count = 1;
 
-	wrapper->header->ref_count = 1;
+	return ++header;
 
-	return wrapper->object;
-	
 error:
-	free(wrapper->header);
-	free(wrapper->object);
-	free(wrapper);
+	free(header);
 	return NULL;
 }
 
@@ -42,15 +29,14 @@ static inline Header *Object_get_header(void *object_ref)
 	return ((Header *)object_ref) - 1;
 }
 
-static inline void Object_release(void *object_ref)
+static inline void Object_release(void **object_ref)
 {
-	Header *header = Object_get_header(object_ref);
+	Header *header = Object_get_header(*object_ref);
 
 	header->ref_count--;
 
 	if(header->ref_count == 0) {
 		free(header);
-		free(object_ref);
 	}
 }
 
