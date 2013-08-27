880bcfca732adb0e9380b29325ddf4de797fca69
Oleg Ivanov <iosmsft@outlook.com>
2013-08-27 10:09:42 +0300

    reference counting

--- src/cmm/cmm.h
+++ src/cmm/cmm.h
@@ -13,6 +13,16 @@ typedef struct Header {
 	uint32_t ref_count;	
 } Header;
 
+static inline Header *Object_get_header(void *object);
+
+static inline void Object_new(size_t object_size, void **object_ref);
+static inline void Object_new_field(size_t field_size, void *object, void **field);
+
+static inline void Object_release(void **object);
+static inline void Object_release_childs(void **object);
+
+static inline void Object_retain(void **lvalue, void **rvalue);
+
 #define OBJECT(Type, Ref) Object_new(sizeof(Type), &(Ref))
 
 #define FIELD(Type, Object, Field) Object_new_field(sizeof(Type), Object, &(Object->Field))
@@ -56,6 +66,17 @@ static inline void Object_release(void **object)
 	if(*object == NULL) return;
 
 	Header *header = Object_get_header(*object);
+	header->ref_count--;
+
+	if(header->ref_count == 0) {
+		Object_release_childs(object);
+		free(header);
+	}
+}
+
+static inline void Object_release_childs(void **object)
+{
+	Header *header = Object_get_header(*object);
 
 	int i = 0;
 	uint32_t bit = 0;
@@ -68,24 +89,18 @@ static inline void Object_release(void **object)
 
 		ref_map = ref_map >> ONE;
 	}
-
-	header->ref_count--;
-
-	if(header->ref_count == 0) {
-		free(header);
-	}
 }
 
-static inline void Object_retain(void **object_left, void **object_right)
+static inline void Object_retain(void **lvalue, void **rvalue)
 {
-	if(*object_right != NULL) {
-		Header *header = Object_get_header(*object_right);
+	if(*rvalue != NULL) {
+		Header *header = Object_get_header(*rvalue);
 		header->ref_count++;
 	}
 
-	Object_release(object_left);
+	Object_release(lvalue);
 
-	*object_left = *object_right;
+	*lvalue = *rvalue;
 }
 
 #endif
