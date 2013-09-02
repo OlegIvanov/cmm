66d6929aa83de4639ce98ced7a216769761eabee
Oleg Ivanov <iosmsft@outlook.com>
2013-09-02 19:54:11 +0300

    ident ptrs

--- src/cmm/cmm.c
+++ src/cmm/cmm.c
@@ -36,30 +36,38 @@ error:
 inline void Object_release(void **obj)
 {
 	void *obj_ptr = *obj;
+
 	if(obj_ptr == NULL) return;
 
 	ObjectHeader *obj_header = Object_get_header(obj_ptr);
 	obj_header->desc->ref_count--;
 
-	if(obj_header->desc->ref_count != 0) return;
+	if(obj_header->desc->ref_count == 0) {
+		BlockHeader *block_header = GC_get_block_header(__GC__, (uintptr_t)obj_ptr);
+		uint32_t object_size_bytes = block_header->size - sizeof(ObjectHeader);
+		void *obj_addr = NULL;
 
-	BlockHeader *block_header = GC_get_block_header(__GC__, (uintptr_t)obj_ptr);
-	uint32_t object_size_bytes = block_header->size - sizeof(ObjectHeader);
-	void *obj_addr = NULL;
+		for(obj_addr = obj_ptr;
+			obj_addr < obj_ptr + object_size_bytes;
+			obj_addr += WORD_SIZE_BYTES) {
 
-	for(obj_addr = obj_ptr;
-		obj_addr < obj_ptr + object_size_bytes;
-		obj_addr += WORD_SIZE_BYTES) {
+			void *ptr_candidate = *(void **)obj_addr;
+			BlockHeader *block_header_candidate = GC_get_block_header(__GC__, (uintptr_t)ptr_candidate);
 
-		void *ptr_candidate = *(void **)obj_addr;
-		BlockHeader *block_header_candidate = GC_get_block_header(__GC__, (uintptr_t)ptr_candidate);
+			if(block_header_candidate) {
+				int16_t block_displ_words = GC_get_block_displ((uintptr_t)ptr_candidate) / WORD_SIZE_BYTES;
 
-		if(block_header_candidate) {
-			int16_t block_displ_words = GC_get_block_displ((uintptr_t)ptr_candidate) / WORD_SIZE_BYTES;
-
-			if(block_header_candidate->map[block_displ_words - 1] == 0) {
-				Object_release(&ptr_candidate);
+				if(block_header_candidate->map[block_displ_words - 1] == 0) {
+					Object_release(&ptr_candidate);
+				}
 			}
 		}
+
+		int sz = GC_get_size(__GC__, block_header->size);
+		List *freelist = __GC__->freelist[sz];
+		List_push(freelist, obj_header);
+		
+		memset(obj_header, 0, block_header->size);
+		free(obj_header->desc);
 	}
 }
